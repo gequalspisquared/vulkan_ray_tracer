e36261f0a38f46ad5a1b9b69edca97636f7fc1f9
Nicholas Crane <nickcrusher30@gmail.com>
2024-05-06 14:35:16 -0600

    Create a vulkan instance with required extensions

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,48 @@ use winit::application::ApplicationHandler;
 use winit::event::{ElementState, KeyEvent, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::keyboard::{Key, NamedKey};
+use winit::raw_window_handle::HasDisplayHandle;
 use winit::window::{Theme, Window};
 
-#[derive(Default)]
+use ash::{vk, Entry};
+use ash_window;
+
 struct App {
     window: Option<Window>,
+    entry: Entry,
+    instance: ash::Instance,
+}
+
+impl App {
+    unsafe fn new() -> App {
+        let (entry, instance) = unsafe { Self::init_vulkan() };
+
+        let extensions = unsafe { entry.enumerate_instance_extension_properties(None).unwrap() };
+        println!("Extensions count: {}", extensions.len()); // 19 on my machine
+
+        App {
+            window: None,
+            entry,
+            instance,
+        }
+    }
+
+    unsafe fn init_vulkan() -> (Entry, ash::Instance) {
+        let entry = Entry::linked();
+        let app_info = vk::ApplicationInfo {
+            api_version: vk::make_api_version(0, 1, 0, 0),
+            ..Default::default()
+        };
+
+        let create_info = vk::InstanceCreateInfo {
+            p_application_info: &app_info,
+            ..Default::default()
+        };
+
+        let instance = unsafe { entry.create_instance(&create_info, None).unwrap() };
+
+        (entry, instance)
+    }
 }
 
 impl ApplicationHandler for App {
@@ -16,8 +53,32 @@ impl ApplicationHandler for App {
             .with_inner_size(winit::dpi::LogicalSize::new(800.0, 800.0))
             .with_title("Vulkan Ray Tracer");
         self.window = Some(event_loop.create_window(window_attributes).unwrap());
+
+        let extensions = ash_window::enumerate_required_extensions(
+            self.window
+                .as_ref()
+                .unwrap()
+                .display_handle()
+                .unwrap()
+                .as_raw(),
+        )
+        .unwrap();
+
+        println!("Required extensions count: {}", extensions.len());
+
+        let app_info = vk::ApplicationInfo {
+            api_version: vk::make_api_version(0, 1, 0, 0),
+            ..Default::default()
+        };
+        let create_info = vk::InstanceCreateInfo {
+            p_application_info: &app_info,
+            pp_enabled_extension_names: extensions.as_ptr(),
+            ..Default::default()
+        };
+        self.instance = unsafe { self.entry.create_instance(&create_info, None).unwrap() };
     }
 
+    // main loop
     fn window_event(
         &mut self,
         event_loop: &winit::event_loop::ActiveEventLoop,
@@ -31,17 +92,22 @@ impl ApplicationHandler for App {
             }
             WindowEvent::RedrawRequested => {
                 self.window.as_ref().unwrap().request_redraw();
-            },
-            WindowEvent::KeyboardInput { 
-                event: KeyEvent { logical_key: key, state: ElementState::Pressed, .. },
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        logical_key: key,
+                        state: ElementState::Pressed,
+                        ..
+                    },
                 ..
             } => match key {
                 Key::Named(NamedKey::Escape) => {
                     event_loop.exit();
                 }
-                _ => ()
-            }
-            _ => ()
+                _ => (),
+            },
+            _ => (),
         }
     }
 }
@@ -50,6 +116,6 @@ fn main() {
     let event_loop = EventLoop::new().unwrap();
     event_loop.set_control_flow(ControlFlow::Poll);
 
-    let mut app = App::default();
+    let mut app = unsafe { App::new() };
     let _ = event_loop.run_app(&mut app);
 }
