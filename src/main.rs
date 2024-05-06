bb4a9967c230d6c43286573244a62abbf7de35c7
Nicholas Crane <nickcrusher30@gmail.com>
2024-05-06 11:18:21 -0600

    Create a blank window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use winit::application::ApplicationHandler;
+use winit::event::{ElementState, KeyEvent, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::keyboard::{Key, NamedKey};
+use winit::window::{Theme, Window};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        let window_attributes = Window::default_attributes()
+            .with_theme(Some(Theme::Dark))
+            .with_inner_size(winit::dpi::LogicalSize::new(800.0, 800.0))
+            .with_title("Vulkan Ray Tracer");
+        self.window = Some(event_loop.create_window(window_attributes).unwrap());
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("Closing window...");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            },
+            WindowEvent::KeyboardInput { 
+                event: KeyEvent { logical_key: key, state: ElementState::Pressed, .. },
+                ..
+            } => match key {
+                Key::Named(NamedKey::Escape) => {
+                    event_loop.exit();
+                }
+                _ => ()
+            }
+            _ => ()
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
+    let _ = event_loop.run_app(&mut app);
 }
